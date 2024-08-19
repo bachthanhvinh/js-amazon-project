56c522841d0983cf1049d36b43236b3458443797
Vinh <bachthanhvinh12@gmail.com>
2024-08-19 15:58:43 +0700

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -32,6 +32,7 @@ export function renderOrderSummary() {
 
     cartSummaryHTML += `
       <div class="cart-item-container
+       js-cart-item-container
         js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date ">
           Delivery date: ${DateString}
@@ -48,7 +49,9 @@ export function renderOrderSummary() {
             <div class="product-price">
               $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity
+             js-product-quantity-${matchingProduct.id}
+            ">
               <span>
                 Quantity: <span class="quantity-label js-quantity-label-${
                   matchingProduct.id
@@ -66,9 +69,10 @@ export function renderOrderSummary() {
               <span class='save-quantity-link link-primary js-save-link'
               data-product-id="${matchingProduct.id}"
               >Save</span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
-                matchingProduct.id
-              }">
+              <span class="delete-quantity-link link-primary js-delete-link
+              js-delete-link-${matchingProduct.id}
+              " 
+              data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
